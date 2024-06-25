bfbf57922550b56e7c87ef1873bc3612b746ebb0
Ghxsted <101996880+ghostedxcx@users.noreply.github.com>
2024-06-25 15:24:35 +0100

    add fivem support

---
+++ build/client/cl_3002.js
@@ -0,0 +1,5 @@
+var a = 1;
+console.log(a);
+function add(a, b) {
+    return a + b;
+}
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,2 +1,6 @@
 const a: number = 1;
 console.log(a);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
